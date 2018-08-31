114cee638db3b51c9e968542f30df201403aa97e
ToLToL <treqh@outlook.com>
2018-08-31 18:55:51 +0200

    Can login/logout via Facebook account

---
+++ Friends/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Friends
+//
+//  Created by ToLToL on 31/08/2018.
+//  Copyright © 2018 Hyokil.KIM/Maxime.GERNATH. All rights reserved.
+//
+
+import UIKit
+import FBSDKLoginKit
+
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        let fbLoginButton = FBSDKLoginButton()
+        
+        fbLoginButton.frame = CGRect(x: 16, y: 50, width: view.frame.width - 32, height: 50)
+        view.addSubview(fbLoginButton)
+        
+        fbLoginButton.delegate = self
+        fbLoginButton.readPermissions = ["email", "user_friends", "public_profile"]
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("Did log out of Facebook")
+    }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        if error != nil {
+            print(error)
+            return
+        }
+        
+        print("Successfully logged in with facebook")
+        FBSDKGraphRequest(graphPath: "/me", parameters: ["fields": "id, name, email"]).start { (connection, result, error) in
+            
+            if error != nil {
+                print("Failed to start graph request:", error!)
+                return
+            }
+    
+            print(result!)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
