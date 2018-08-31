924b45f8737c32ecb63dbee447efb3a2993ed160
ToLToL <treqh@outlook.com>
2018-08-31 21:33:59 +0200

    Can display one's friends names, id and email (only those who used the app)

--- Friends/ViewController.swift
+++ Friends/ViewController.swift
@@ -8,26 +8,46 @@
 
 import UIKit
 import FBSDKLoginKit
+import FBSDKShareKit
+import FBSDKCoreKit
 
 class ViewController: UIViewController, FBSDKLoginButtonDelegate {
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
        
         let fbLoginButton = FBSDKLoginButton()
+        let inviteFriendsbutton: UIButton =  {
+        
+            let button = UIButton(type: .system)
+            button.backgroundColor = .green
+            button.frame = CGRect(x: 16, y: 116, width: view.frame.width - 32, height: 50)
+            button.setTitle("Invite Friends", for: .normal)
+            button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+            button.setTitleColor(.white, for: .normal)
+            return button
+        }()
         
         fbLoginButton.frame = CGRect(x: 16, y: 50, width: view.frame.width - 32, height: 50)
         view.addSubview(fbLoginButton)
+        view.addSubview(inviteFriendsbutton)
         
         fbLoginButton.delegate = self
         fbLoginButton.readPermissions = ["email", "user_friends", "public_profile"]
+        fbLoginButton.loginBehavior = .web
     }
     
+    
     func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        
+        //FBSDKAccessToken.setCurrent(nil)
+        //FBSDKProfile.setCurrent(nil)
         print("Did log out of Facebook")
     }
     
-    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+    func loginButton(_ logeeeeeeeeeeeeeeeeeeeeeeereeeeeeeeeeeeeeeinButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        
         if error != nil {
             print(error)
             return
@@ -42,9 +62,20 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
             }
     
             print(result!)
+            
+            FBSDKGraphRequest(graphPath: "/me/friends", parameters: ["fields": "id, first_name, last_name, name"]).start(completionHandler: { (connection, result, error) in
+                
+                if error != nil {
+                    print("Failed to start graph request:", error!)
+                    return
+                }
+                
+                let data = result as! [String: Any]
+                print(data)
+            })
         }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
