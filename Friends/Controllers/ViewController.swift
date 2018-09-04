18549142e9b5dd145a39a5faf60cbe62b1ca9525
ToLToL <treqh@outlook.com>
2018-09-05 00:06:23 +0200

    Display the WelcomeViewController only when the facebook login button is on status log in

--- Friends/Controllers/ViewController.swift
+++ Friends/Controllers/ViewController.swift
@@ -18,55 +18,11 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //MARK: - Facebook login button settings -
+        
         facebookLoginButton.delegate = self
         facebookLoginButton.readPermissions = ["email", "user_friends", "public_profile"]
         facebookLoginButton.loginBehavior = .web
-        
-        //MARK: - INIT BUTTONS -
-        
-        /*let fbLoginButton = FBSDKLoginButton()
-        
-        fbLoginButton.frame = CGRect(x: 16, y: 50, width: view.frame.width - 32, height: 50)
-        
-        let displayFriendsInfoButton: UIButton =  {
-        
-            let button = UIButton(type: .system)
-            
-            button.backgroundColor = .green
-            button.frame = CGRect(x: 16, y: 116, width: view.frame.width - 32, height: 50)
-            button.setTitle("Display Friends Info", for: .normal)
-            button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
-            button.setTitleColor(.white, for: .normal)
-            button.addTarget(self, action: #selector(displayFriendsInfo), for: .touchUpInside)
-            
-            return button
-        }()
-        
-        let displayFriendsTabViewButton: UIButton =  {
-            
-            let button = UIButton(type: .system)
-            
-            button.backgroundColor = .green
-            button.frame = CGRect(x: 16, y: 182, width: view.frame.width - 32, height: 50)
-            button.setTitle("Display Friends Tab View", for: .normal)
-            button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
-            button.setTitleColor(.black, for: .normal)
-            button.addTarget(self, action: #selector(displayFriendsTabView), for: .touchUpInside)
-            
-            return button
-        }()
-        
-        //MARK: - ADD SUBVIEWS -
-        
-        view.addSubview(fbLoginButton)
-        view.addSubview(displayFriendsInfoButton)
-        view.addSubview(displayFriendsTabViewButton)
-        
-        //MARK: - OTHER SETTINGS -
-        
-        fbLoginButton.delegate = self
-        fbLoginButton.readPermissions = ["email", "user_friends", "public_profile"]
-        fbLoginButton.loginBehavior = .web*/
     }
     
     //MARK: - Display Info Methods -
@@ -83,6 +39,24 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
         FacebookSDK.request(path: "/me/friends", parameters: ["fields": "picture.width(250).height(250), id, first_name, last_name, name"]) { (result) in
             print(result)
         }
+    }
+
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        
+        // push WelcomeViewController only when the facebookLogginButton is on "Log in" Status
+        
+        if identifier == "pushWelcomeViewController" {
+            
+            let buttonStatus = facebookLoginButton.currentTitle
+            
+            if buttonStatus == "Log in" {
+                return true
+            }
+            
+            return false
+        }
+        
+        return true
     }
     
     //MARK: - Facebook Authentification Methods -
@@ -103,6 +77,8 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
         FacebookSDK.request(path: "/me", parameters: ["fields": "id, name, email"]) { (result) in
             print(result)
         }
+        
+        performSegue(withIdentifier: "pushWelcomeViewController", sender: self)
     }
 }
 
